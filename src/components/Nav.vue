06a8c9a86f66b88904d83670c02bb7d634617642
Yandhi <15877410869@163.com>
2021-12-26 00:04:16 +0800

    添加 icon 样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels"/>
       标签
     </router-link>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -26,7 +26,7 @@ export default {
 <style lang="scss" scoped>
 nav {
   display: flex;
-  box-shadow: 0 0 3px rgba(0 0 0.9);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   flex-direction: row;
   font-size: 12px;
 
@@ -42,6 +42,10 @@ nav {
       width: 32px;
       height: 32px;
     }
+  }
+
+  > .item.selected {
+    color: #002af7;
   }
 }
 </style>
