61f5622e6fd4b1b214106c32db9bdda249b525dd
Yandhi <15877410869@163.com>
2021-12-27 19:44:20 +0800

    完成 Types.vue 组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -13,16 +13,11 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Prop} from 'vue-property-decorator';
 
-@Component({
-  props: {
-    propMessage: String
-  }
-})
+@Component
 export default class Types extends Vue {
   type = '-';  // '-' 表示支出，'+' 表示收入
-  helloMsg = 'Hello,' + this.propMessage;
 
   selectType(type: string) {
     if (type !== '-' && type !== '+') {
@@ -30,7 +25,7 @@ export default class Types extends Vue {
     } else {
       this.type = type;
     }
-  }
+  };
 
 };
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad/>
-    <Types xxx="hi"/>
+    <Types :xxx="123"/>
     <Notes/>
     <Tags/>
   </Layout>
