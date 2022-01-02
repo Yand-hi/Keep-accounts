57b74980b83363108fcdfa50956eb60c98522cd0
Yandhi <15877410869@163.com>
2022-01-02 23:16:31 +0800

    模块化store

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,31 +1,9 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-  //  record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => recordListModel.create(record),
-
-
-  //  tag store
-  tagList: tagListModel.fetch(),
-  findTag(id: string) {
-    return this.tagList.filter(item => item.id === id)[0];
-  },
-  createTag: (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
-      window.alert('该标签已存在');
-    } else if (message === 'success') {
-      window.alert('添加标签成功');
-    }
-  },
-  removeTag: (id: string) => {
-    return tagListModel.remove(id);
-  },
-  updateTag: (id: string, name: string) => {
-    return tagListModel.update(id, name);
-  },
+  ...recordStore,
+  ...tagStore,
 };
 
 export default store;
