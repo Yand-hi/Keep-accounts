740337e1edba66cd4d0b6eba9de5a3dcba132792
Yandhi <15877410869@163.com>
2022-01-05 01:36:17 +0800

    在 Statistics.vue 中使用 Tabs

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,16 +1,49 @@
 <template>
   <Layout>
-    <p>我是 Statistics</p>
+    <Tabs :data-source="typeList"
+          :value.sync="type"
+          class-prefix="type"/>
+    <Tabs :data-source="intervalList"
+          :value.sync="interval"
+          class-prefix="interval"/>
   </Layout>
 </template>
 
 <script lang="ts">
 
-export default {
-  name: 'Statistics',
-};
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import Types from '@/components/Money/Types.vue';
+import Tabs from '@/components/Tabs.vue';
+
+@Component({
+  components: {Tabs, Types}
+})
+export default class Statistics extends Vue {
+  type = '-';
+  interval = 'day';
+  intervalList = [
+    {text: '按天', value: 'day'},
+    {text: '按周', value: 'week'},
+    {text: '按月', value: 'month'},
+  ];
+  typeList = [
+    {text: '支出', value: '-'},
+    {text: '收入', value: '+'},
+  ];
+}
 </script>
 
 <style lang="scss" scoped>
+::v-deep .type-tabs-item {
+  background: white;
 
+  &.selected {
+    background: #c4c4c4;
+
+    &::after {
+      display: none;
+    }
+  }
+}
 </style>
