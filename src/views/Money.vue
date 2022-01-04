7c7328d86106a37809163d60105198eac898c692
Yandhi <15877410869@163.com>
2022-01-04 21:41:48 +0800

    所有 computed 改成 get 函数

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -21,13 +21,12 @@ import {Component} from 'vue-property-decorator';
 
 @Component({
   components: {NumberPad, Types, FormItem, Tags},
-  computed: {
-    recordList() {
-      return this.$store.state.recordList;
-    }
-  }
 })
 export default class Money extends Vue {
+  get recordList() {
+    return this.$store.state.recordList;
+  }
+
   record: RecordItem = {tag: [], notes: '', type: '-', amount: 0};
 
   created() {
