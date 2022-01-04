7f4ec81e4b775b407c17dc735342fa5bf1cc88ea
Yandhi <15877410869@163.com>
2022-01-05 02:06:15 +0800

    在 Money.vue 中使用 Tabs.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,8 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-    <Types :value.sync="record.type"/>
+    <Tabs :data-source="recordTypeList"
+          :value.sync="record.type"/>
     <div class="notes">
       <FormItem file-name="备注:"
                 placeholder="请输入备注"
@@ -14,19 +15,21 @@
 <script lang="ts">
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
+import Tabs from '@/components/Tabs.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
+import recordTypeList from '@/constants/recordTypeList';
 
 @Component({
-  components: {NumberPad, Types, FormItem, Tags},
+  components: {Tabs, NumberPad, FormItem, Tags},
 })
 export default class Money extends Vue {
   get recordList() {
     return this.$store.state.recordList;
   }
 
+  recordTypeList = recordTypeList;
   record: RecordItem = {tag: [], notes: '', type: '-', amount: 0};
 
   created() {
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <Tabs :data-source="typeList"
+    <Tabs :data-source="recordTypeList"
           :value.sync="type"
           class-prefix="type"/>
     <Tabs :data-source="intervalList"
@@ -13,24 +13,18 @@
 
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
-import Types from '@/components/Money/Types.vue';
 import Tabs from '@/components/Tabs.vue';
+import intervalList from '@/constants/intervalList';
+import recordTypeList from '@/constants/recordTypeList';
 
 @Component({
-  components: {Tabs, Types}
+  components: {Tabs}
 })
 export default class Statistics extends Vue {
   type = '-';
   interval = 'day';
-  intervalList = [
-    {text: '按天', value: 'day'},
-    {text: '按周', value: 'week'},
-    {text: '按月', value: 'month'},
-  ];
-  typeList = [
-    {text: '支出', value: '-'},
-    {text: '收入', value: '+'},
-  ];
+  intervalList = intervalList;
+  recordTypeList = recordTypeList;
 }
 </script>
 
