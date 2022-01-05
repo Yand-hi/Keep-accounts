8eb736e944471a4de8e02f8069b66dd042aad83b
Yandhi <15877410869@163.com>
2022-01-05 22:45:26 +0800

    update css

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -29,20 +29,22 @@ export default class Statistics extends Vue {
 </script>
 
 <style lang="scss" scoped>
-::v-deep .type-tabs-item {
-  background: white;
+::v-deep {
+  .type-tabs-item {
+    background: white;
 
-  &.selected {
-    background: #c4c4c4;
+    &.selected {
+      background: #c4c4c4;
 
-    &::after {
-      display: none;
+      &::after {
+        display: none;
+      }
     }
   }
-}
 
-::v-deep .interval-tabs-item {
-  height: 48px;
-  font-size: 18px;
+  .interval-tabs-item {
+    height: 48px;
+    font-size: 18px;
+  }
 }
 </style>
