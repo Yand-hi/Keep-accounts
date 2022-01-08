7fe24ad498a9bc237cefe19888248ba37d43e33b
Yandhi <15877410869@163.com>
2022-01-09 01:11:30 +0800

    提示用户使用手机进行预览，添加二维码

--- src/main.ts
+++ src/main.ts
@@ -23,3 +23,16 @@ window.onload = function () {
     window.scrollTo(0, 999);
   }, 0);
 };
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('温馨提示：请使用手机扫码查看，以保证预览效果~');
+  const img = document.createElement('img');
+  img.src = '/keep-accounts.png';
+  img.style.width = '360px';
+  img.style.position = 'fixed';
+  img.style.top = '50%';
+  img.style.left = '50%';
+  img.style.transform = 'translate(-50%, -50%)';
+  img.style.boxShadow = '0 0 10px raga(0,0,0,0.25)';
+  document.body.appendChild(img);
+}
