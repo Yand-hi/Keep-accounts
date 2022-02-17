ac9e8015d2e8b0cff899c99b74cb0b0c07fb9c3a
Yandhi <15877410869@163.com>
2022-02-17 23:01:50 +0800

    change messageBox style of mobile

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -11,6 +11,8 @@ export class TagHelper extends Vue {
     this.$prompt('请输入标签名', '提示', {
       confirmButtonText: '确定',
       cancelButtonText: '取消',
+      showClose: false,
+      customClass: 'messageBox'
     }).then(res => {
       // @ts-ignore
       const name = res.value
@@ -24,6 +26,8 @@ export class TagHelper extends Vue {
       if (this.$store.state.createTagError) {
         this.$message.warning(map[this.$store.state.createTagError.message] || '未知错误');
       }
+    }).catch(err => {
+      console.error(err)
     })
   }
 }
